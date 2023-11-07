697067d644ade182d1ab9c4814baabd9886f54da
Kandunuri <saikiran.kandunuri@capgemini.com>
2023-11-07 16:34:36 +0530

    EMI and Loan Entities

--- src/main/java/com/hla/in/homeloanapplication/entities/LoanAgreement.java
+++ src/main/java/com/hla/in/homeloanapplication/entities/LoanAgreement.java
@@ -1,5 +1,24 @@
 package com.hla.in.homeloanapplication.entities;
 
 public class LoanAgreement {
+	private long loanAgreementId;
+	private long loanApplication;
+	public LoanAgreement(long loanAgreementId, long loanApplication) {
+		super();
+		this.loanAgreementId = loanAgreementId;
+		this.loanApplication = loanApplication;
+	}
+	public long getLoanAgreementId() {
+		return loanAgreementId;
+	}
+	public void setLoanAgreementId(long loanAgreementId) {
+		this.loanAgreementId = loanAgreementId;
+	}
+	public long getLoanApplication() {
+		return loanApplication;
+	}
+	public void setLoanApplication(long loanApplication) {
+		this.loanApplication = loanApplication;
+	}
 
 }
